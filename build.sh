89d870bde5d79e8acb93947c78fbfa0a9188a6de
wojciechklicki <wojciech.klicki@gmail.com>
2017-08-22 17:51:47 +0200

    Old build file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ./javaagent
-javac -source 1.6 -target 1.6 com/klicki/jagent/*.java
-jar cvfm timing_agent.jar manifest.txt ./com/klicki/jagent/*.class
-cd ../example
-javac Example.java
-java -javaagent:../javaagent/timing_agent.jar Example
-cd ..
-cp ./javaagent/timing_agent.jar ~/shares/domino/xsp/
